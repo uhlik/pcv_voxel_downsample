60848a82af1b65f555a7d20eb15f78a7933216b1
Jakub Uhlik <jakub.uhlik@gmail.com>
2026-09-02 14:32:59 +0200

    build

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
-SETUP_DIR = os.path.abspath(os.path.dirname(__file__))
-
 if sys.platform == "win32":
     extra_compile_args = ["/O2", "/std:c++14"]
 else:
@@ -15,10 +13,7 @@ extensions = [
     Extension(
         name="pcv_voxel_downsample._voxel_downsample",
         sources=["src/pcv_voxel_downsample/_voxel_downsample.pyx"],
-        include_dirs=[
-            np.get_include(), 
-            os.path.join(SETUP_DIR, "src", "pcv_voxel_downsample", "_voxel_downsample.hpp")
-        ],
+        include_dirs=[np.get_include(), "src/pcv_voxel_downsample"],
         language="c++",
         extra_compile_args=extra_compile_args,
     )
